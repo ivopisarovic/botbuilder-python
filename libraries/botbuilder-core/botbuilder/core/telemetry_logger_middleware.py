36772cec0255804c50b9cd5d9307594b496e22f5
Ivo Pisařovic <ivo@pisarovic.cz>
2022-04-22 18:46:53 +0200

    Update telemetry_logger_middleware.py

--- libraries/botbuilder-core/botbuilder/core/telemetry_logger_middleware.py
+++ libraries/botbuilder-core/botbuilder/core/telemetry_logger_middleware.py
@@ -302,11 +302,6 @@ class TelemetryLoggerMiddleware(Middleware):
         if activity.channel_id == Channels.ms_teams:
             teams_channel_data: TeamsChannelData = activity.channel_data
                 
-            print('AA-')
-            print(activity.channel_data)
-            print('BB-')
-            print(teams_channel_data)
-
             properties["TeamsTenantId"] = (
                 teams_channel_data['tenant']['id']
                 if teams_channel_data and teams_channel_data.get("tenant", {}).get("id", None)
